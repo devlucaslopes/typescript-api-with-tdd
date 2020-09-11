371ff0b3a922e6533d10a91113b555a2bcc23a83
Lucas Lopes <dev.thuram@gmail.com>
2020-09-11 11:57:09 -0300

    refactor: add bcrypt adapter on CreateUserService

--- src/main/factories/useCases/user/CreateUserFactory.ts
+++ src/main/factories/useCases/user/CreateUserFactory.ts
@@ -1,7 +1,11 @@
-import { CreateUser } from '@/data/useCases/user/Createuser';
+import { CreateUser } from '@/data/useCases/users/CreateUser';
 import UserRepository from '@/infra/typeorm/repositories/UserRepository';
+import { BcryptAdapter } from '@/infra/criptograpgy/bcrypt/BcryptAdapter';
 
 export const makeCreateUser = (): CreateUser => {
+  const SALT = 12;
+  const bcryptAdapter = new BcryptAdapter(SALT);
   const userRepository = new UserRepository();
-  return new CreateUser(userRepository);
+
+  return new CreateUser(userRepository, bcryptAdapter);
 };
