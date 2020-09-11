ccc995eeb55038d3d0ec78ef44a39b79ee4f8c36
Lucas Lopes <dev.thuram@gmail.com>
2020-09-11 11:52:15 -0300

    feat: returns hashed value on success

--- src/infra/criptograpgy/bcrypt/BcryptAdapter.ts
+++ src/infra/criptograpgy/bcrypt/BcryptAdapter.ts
@@ -1,11 +1,12 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { IHasher } from '@/data/protocols/cryptography/Hasher';
 
 export class BcryptAdapter implements IHasher {
   constructor(private readonly salt: number) {}
 
   async hash(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
-    return Promise.resolve('value');
+    const hashedValue = await bcrypt.hash(value, this.salt);
+
+    return hashedValue;
   }
 }
