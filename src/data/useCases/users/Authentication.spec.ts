21ea62c0c8837fae0e2c2ef131745d50bc46d8cd
Lucas Lopes <dev.thuram@gmail.com>
2020-09-15 15:13:45 -0300

    test: ensure returns undefined if hasher.compare fails

--- src/data/useCases/users/Authentication.spec.ts
+++ src/data/useCases/users/Authentication.spec.ts
@@ -1,19 +1,28 @@
+import { FakeHasher } from '@/data/protocols/cryptography/fakes/FakeHasher';
 import { FakeUserRepository } from '@/data/protocols/database/users/fakes/FakeUserRepository';
 import { IAuthentication } from '@/domain/useCases/users/Authentication';
 import { Authentication } from './Authentication';
 
-let authentication: IAuthentication;
+let fakeHasher: FakeHasher;
 let fakeUserRepository: FakeUserRepository;
+let authentication: IAuthentication;
+
+const makeFakeUser = () => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_value',
+});
 
 const makeFakeRequest = () => ({
   email: 'any_email@mail.com',
-  password: 'password',
+  password: 'any_password',
 });
 
 describe('# Authentication use case', () => {
   beforeAll(() => {
+    fakeHasher = new FakeHasher();
     fakeUserRepository = new FakeUserRepository();
-    authentication = new Authentication(fakeUserRepository);
+    authentication = new Authentication(fakeUserRepository, fakeHasher);
   });
 
   it('should calls UserRepositoy.findByEmail with correct email', async () => {
@@ -25,6 +34,28 @@ describe('# Authentication use case', () => {
   });
 
   it('should return undefined if user not found', async () => {
+    const response = await authentication.execute(makeFakeRequest());
+
+    expect(response).toBeUndefined();
+  });
+
+  it('should calls Hasher.compare with correct value', async () => {
+    fakeUserRepository.create(makeFakeUser());
+
+    const compareSpy = jest.spyOn(fakeHasher, 'compare');
+
+    await authentication.execute(makeFakeRequest());
+
+    expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_value');
+  });
+
+  it('should returns undefined if Hasher.compare fails', async () => {
+    fakeUserRepository.create(makeFakeUser());
+
+    jest
+      .spyOn(fakeHasher, 'compare')
+      .mockReturnValueOnce(Promise.resolve(false));
+
     const response = await authentication.execute(makeFakeRequest());
 
     expect(response).toBeUndefined();
