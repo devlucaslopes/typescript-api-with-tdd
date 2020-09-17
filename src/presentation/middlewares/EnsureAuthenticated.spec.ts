8920588d1b716aea360b08b33cec1c33786cff47
Lucas Lopes <dev.thuram@gmail.com>
2020-09-17 10:13:54 -0300

    test: ensure token is valid

--- src/presentation/middlewares/EnsureAuthenticated.spec.ts
+++ src/presentation/middlewares/EnsureAuthenticated.spec.ts
@@ -1,5 +1,6 @@
 import { FakeDecrypter } from '@/data/protocols/cryptography/fakes/FakeDecrypter';
 import { AccessDeniedError } from '../errors/AccessDeniedError';
+import { InvalidAccessTokenError } from '../errors/InvalidAccessTokenError';
 import { forbidden, success } from '../helpers/HttpHelper';
 import { EnsureAuthenticated } from './EnsureAuthenticated';
 
@@ -12,27 +13,49 @@ describe('# EnsureAuthenticated', () => {
     ensureAuthenticated = new EnsureAuthenticated(fakeDecrypter);
   });
 
-  it('should return 403 if authorization header not provided', async () => {
+  it('should returns 403 if authorization header not provided', async () => {
     const response = await ensureAuthenticated.handle({});
 
     expect(response).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it('should returns 403 if access token is invalid', async () => {
+    const response = await ensureAuthenticated.handle({
+      headers: {
+        authorization: 'invalid_token',
+      },
+    });
+
+    expect(response).toEqual(forbidden(new InvalidAccessTokenError()));
   });
 
   it('should calls Decrypter.decrypt with correct values', async () => {
     const decryptSpy = jest.spyOn(fakeDecrypter, 'decrypt');
 
     await ensureAuthenticated.handle({
-      headers: { authorization: 'any_token' },
+      headers: { authorization: 'Bearer any_token' },
     });
 
     expect(decryptSpy).toHaveBeenCalledWith('any_token');
   });
 
-  it('should return userId on success', async () => {
+  it('should returns userId on success', async () => {
     const response = await ensureAuthenticated.handle({
-      headers: { authorization: 'any_token' },
+      headers: { authorization: 'Bearer any_token' },
     });
 
     expect(response).toEqual(success({ userId: 'any_id' }));
+  });
+
+  it('should returns 400 if userId is undefined', async () => {
+    jest
+      .spyOn(fakeDecrypter, 'decrypt')
+      .mockReturnValueOnce(Promise.resolve(undefined));
+
+    const response = await ensureAuthenticated.handle({
+      headers: { authorization: 'Bearer any_token' },
+    });
+
+    expect(response).toEqual(forbidden(new InvalidAccessTokenError()));
   });
 });
