5564aa1c22a6cf90e726381c875ddaeacf0eb77b
Lucas Lopes <dev.thuram@gmail.com>
2020-09-17 21:55:07 -0300

    test: ensure forgot password return 200 on success

---
+++ src/main/routes/users/forgotPassword.test.ts
@@ -0,0 +1,44 @@
+import supertest from 'supertest';
+import { TypeORMHelper } from '@/infra/typeorm/helpers/TypeORMHelper';
+import UserRepository from '@/infra/typeorm/repositories/UserRepository';
+import { hash } from 'bcrypt';
+
+let request: supertest.SuperTest<supertest.Test>;
+
+describe('# ForgotPassword route', () => {
+  beforeAll(async () => {
+    await TypeORMHelper.connect();
+    const { app } = await import('@/main/config/app');
+
+    request = supertest(app);
+  });
+
+  afterAll(async () => {
+    await TypeORMHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    await TypeORMHelper.clear();
+  });
+
+  it('should return 200 on success', async () => {
+    const hashedPassword = await hash('123456', 12);
+
+    const userData = {
+      name: 'lucas',
+      email: 'lucas@mail.com',
+      password: hashedPassword,
+    };
+
+    const userRepository = new UserRepository();
+
+    await userRepository.create(userData);
+
+    await request
+      .post('/forgot-password')
+      .send({
+        email: 'lucas@mail.com',
+      })
+      .expect(200);
+  });
+});
