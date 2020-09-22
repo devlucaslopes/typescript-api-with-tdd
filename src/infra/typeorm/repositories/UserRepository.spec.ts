db0be3a320af3ff14432f70ad5b83a214f5ef429
Lucas Lopes <dev.thuram@gmail.com>
2020-09-22 14:17:43 -0300

    test: user repository coverage

--- src/infra/typeorm/repositories/UserRepository.spec.ts
+++ src/infra/typeorm/repositories/UserRepository.spec.ts
@@ -17,35 +17,80 @@ describe('# UserRepository', () => {
     await TypeORMHelper.clear();
   });
 
-  it('should create a new user', async () => {
-    const user = await userRepository.create({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
-    });
+  describe('create()', () => {
+    it('should create a new user', async () => {
+      const user = await userRepository.create({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      });
 
-    expect(user.id).toBeTruthy();
-    expect(user.name).toBe('any_name');
-    expect(user.email).toBe('any_email@mail.com');
+      expect(user.id).toBeTruthy();
+      expect(user.name).toBe('any_name');
+      expect(user.email).toBe('any_email@mail.com');
+    });
   });
 
-  it('should returns user find by email address', async () => {
-    const user = await userRepository.create({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+  describe('save()', () => {
+    it('should save a user', async () => {
+      const user = await userRepository.create({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      });
+
+      user.password = 'new_password';
+
+      const updatedUser = await userRepository.save(user);
+
+      expect(updatedUser.id).toBe(user.id);
+      expect(updatedUser.password).toBe('new_password');
     });
-
-    const userExists = await userRepository.findByEmail(user.email);
-
-    expect(userExists?.id).toBeTruthy();
-    expect(userExists?.name).toBe('any_name');
-    expect(userExists?.email).toBe('any_email@mail.com');
   });
 
-  it('should returns undefined if user not found', async () => {
-    const userExists = await userRepository.findByEmail('any_email@mail.com');
+  describe('findByEmail()', () => {
+    it('should returns undefined if user not found', async () => {
+      const userExists = await userRepository.findByEmail('any_email@mail.com');
 
-    expect(userExists).toBeUndefined();
+      expect(userExists).toBeUndefined();
+    });
+
+    it('should returns user find by email address', async () => {
+      const user = await userRepository.create({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      });
+
+      const userExists = await userRepository.findByEmail(user.email);
+
+      expect(userExists?.id).toBeTruthy();
+      expect(userExists?.name).toBe('any_name');
+      expect(userExists?.email).toBe('any_email@mail.com');
+    });
+  });
+
+  describe('findById()', () => {
+    it('should returns user find by id', async () => {
+      const user = await userRepository.create({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      });
+
+      const userExists = await userRepository.findById(user.id);
+
+      expect(userExists?.id).toBe(user.id);
+      expect(userExists?.name).toBe('any_name');
+      expect(userExists?.email).toBe('any_email@mail.com');
+    });
+
+    it('should returns undefined if user not found', async () => {
+      const userExists = await userRepository.findById(
+        'df7ca51e-66ce-4cf0-9fb1-4a82ad64acef',
+      );
+
+      expect(userExists).toBeUndefined();
+    });
   });
 });
