9dee3a7fbebc2f1c3d323b68aa8914ce78cc117d
Lucas Lopes <dev.thuram@gmail.com>
2020-09-22 08:59:36 -0300

    test: ensure hasher is called with correct value

--- src/data/useCases/users/ResetPassword.spec.ts
+++ src/data/useCases/users/ResetPassword.spec.ts
@@ -1,19 +1,23 @@
+import { FakeHasher } from '@/data/protocols/cryptography/fakes/FakeHasher';
 import { FakeUserRepository } from '@/data/protocols/database/users/fakes/FakeUserRepository';
 import { FakeUserTokenRepository } from '@/data/protocols/database/users/fakes/FakeUserTokenRepository';
 import { ResetPassword } from './ResetPassword';
 
+let fakeHasher: FakeHasher;
 let fakeUserRepository: FakeUserRepository;
 let fakeUserTokenRepository: FakeUserTokenRepository;
 let resetPassword: ResetPassword;
 
 describe('# ResetPassword', () => {
   beforeAll(() => {
+    fakeHasher = new FakeHasher();
     fakeUserRepository = new FakeUserRepository();
     fakeUserTokenRepository = new FakeUserTokenRepository();
 
     resetPassword = new ResetPassword(
       fakeUserTokenRepository,
       fakeUserRepository,
+      fakeHasher,
     );
   });
 
@@ -54,5 +58,24 @@ describe('# ResetPassword', () => {
     });
 
     expect(findByIdSpy).toHaveBeenCalledWith('any_id');
+  });
+
+  it('should calls Hasher.hash with correct value', async () => {
+    const user = await fakeUserRepository.create({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+
+    fakeUserTokenRepository.create(user.id);
+
+    const hashSpy = jest.spyOn(fakeHasher, 'hash');
+
+    await resetPassword.execute({
+      token: 'valid_token',
+      password: 'new_password',
+    });
+
+    expect(hashSpy).toHaveBeenCalledWith('new_password');
   });
 });
