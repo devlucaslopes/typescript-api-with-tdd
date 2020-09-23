9d7adace14e56b379171bd4ab9feefd67acb8377
Lucas Lopes <dev.thuram@gmail.com>
2020-09-23 08:52:18 -0300

    feat: add try catch on handle method

--- src/presentation/controllers/users/resetPassword/ResetPasswordController.ts
+++ src/presentation/controllers/users/resetPassword/ResetPasswordController.ts
@@ -1,6 +1,7 @@
 import { ResetPassword } from '@/data/useCases/users/ResetPassword';
 import { InvalidResetTokenError } from '@/presentation/errors/InvalidResetTokenError';
-import { badRequest } from '@/presentation/helpers/HttpHelper';
+import { ServerError } from '@/presentation/errors/ServerError';
+import { badRequest, serverError } from '@/presentation/helpers/HttpHelper';
 import { IController } from '@/presentation/protocols/controller';
 import { IRequest, IResponse } from '@/presentation/protocols/http';
 
@@ -8,14 +9,18 @@ export class ResetPasswordController implements IController {
   constructor(private readonly resetPassword: ResetPassword) {}
 
   async handle(request: IRequest): Promise<IResponse> {
-    const { token, password } = request.body;
+    try {
+      const { token, password } = request.body;
 
-    const user = await this.resetPassword.execute({ token, password });
+      const user = await this.resetPassword.execute({ token, password });
 
-    if (!user) {
-      return badRequest(new InvalidResetTokenError());
+      if (!user) {
+        return badRequest(new InvalidResetTokenError());
+      }
+
+      return { statusCode: 200, body: {} };
+    } catch (error) {
+      return serverError(new ServerError(error));
     }
-
-    return { statusCode: 200, body: {} };
   }
 }
